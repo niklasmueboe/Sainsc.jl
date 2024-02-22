ae752682247446b429a54e27fe93ed1e6d0cb079
niklasmueboe <niklasmueboe@github.com>
2024-02-22 16:09:07 +0100

    speedup findlocalmax

--- src/KDE.jl
+++ src/KDE.jl
@@ -13,12 +13,44 @@ function kde(arr, kernel)
     imfilter(collect(arr), kernel, Fill(zero(eltype(arr))))
 end
 
-function findlocalmaxima(sp_c, d::Integer)
-    window = Tuple(2d + 1 for _ = 1:length(size(first(sp_c))))
-    total_rna = totalrna(sp_c)
-    total_rna_max =
-        mapwindow(maximum, total_rna, window, border = Fill(zero(eltype(total_rna)))) # TODO slow
-    findall(.!iszero.(total_rna) .& (total_rna .== total_rna_max))
+function findlocalmaxima(sp_c, d::Integer, kernel)
+    function islocalmax(
+        arr::Matrix,
+        x::Integer,
+        y::Integer,
+        s::Integer,
+        n::Integer,
+        m::Integer,
+    )
+        for i = -s:s
+            xi = x + i
+            if xi < 1 || xi > n
+                continue
+            end
+            for j = -s:s
+                yj = y + j
+                if yj < 1 || yj > m
+                    continue
+                elseif @inbounds arr[x, y] < arr[xi, yj]
+                    return false
+                end
+            end
+        end
+        return true
+    end
+
+    total_rna = kde(totalrna(sp_c), kernel) |> collect
+
+    n, m = size(total_rna)
+    max_coordinates = CartesianIndex{2}[]
+
+    for x = 1:n, y = 1:m
+        if !iszero(total_rna[x, y]) && islocalmax(total_rna, x, y, d, n, m)
+            push!(max_coordinates, CartesianIndex(x, y))
+        end
+    end
+
+    max_coordinates
 end
 
 function assigncelltype(sp_c, signatures; kwargs...)
