3bfcc58cd5a4c870a4df92e400be929f3f948e77
niklasmueboe <niklasmueboe@github.com>
2024-06-24 10:22:38 +0200

    fix SingleCellProjections extension

--- ext/SingleCellProjectionsExt.jl
+++ ext/SingleCellProjectionsExt.jl
@@ -5,11 +5,11 @@ using SingleCellProjections: DataMatrix
 using Sainsc
 
 function Sainsc.getlocalmaxima(::Type{DataMatrix}, counts, localmax, kernel; genes=nothing)
-    return DataMatrix(getlocalmaxima(counts, localmax, kernel; genes=genes))
+    return DataMatrix(getlocalmaxima(counts, localmax, kernel; genes=genes)...)
 end
 
 function Sainsc.readstereoseqbinned(::Type{DataMatrix}, file, binsize::Integer)
-    return DataMatrix(readstereoseqbinned(file, binsize))
+    return DataMatrix(readstereoseqbinned(file, binsize)...)
 end
 
 end # module SingleCellProjectionsExt
